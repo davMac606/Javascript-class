5d9831d0f3fbd6b58ffc4cc4976c0354748ff62b
davMac606 <cl201238@g.unicamp.br>
2023-03-01 08:10:36 -0300

    inicio

---
+++ aula5.php
@@ -0,0 +1,21 @@
+<?php
+
+$n = 1;
+$tabuada= 7;
+while($n<=10){
+    echo $n. "x" .$tabuada . "=" . ($n * $tabuada) . "<br>";
+    $n++;
+
+
+$n = 11;
+$tabuada= 4; 
+
+do{
+    echo $n. "x" .$tabuada . "=" . ($n * $tabuada) . "<br>";
+    $n++;
+
+}while($n<=10);
+//do while faz pelo menos uma vez
+
+
+}
